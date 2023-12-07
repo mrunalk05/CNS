e74aa375f38f2da4007b5467825e0c6ee4c9d695
Mrunal Khade <83811490+mrunalk05@users.noreply.github.com>
2023-12-07 09:29:59 +0530

    Create rsa_fact.cpp

---
+++ rsa_fact.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m = 1e9+7;
+pair<long long, long long> factorize(long long n){
+    for(long long i=2; i*i<=n; i++){
+        if(n % i == 0){
+            return {i,n/i};
+        }
+    }
+    return {0,0};
+}
+// 1000000014000000049
+// 999999943999999559
+
+int main()
+{
+    cout<<"Enter long number which is multiplication of 2 prime numbers :";
+    long long n;
+    cin>>n;
+
+    auto ans = factorize(n);
+    if(ans.first == 0 && ans.second == 0){
+        cout<<"Enter number which is multiplication of 2 prime numbers \n";
+    }else{
+        cout << 1LL*n << " is multiplication of "<<ans.first << " and "<<ans.second<<endl;
+    }
+    return 0;
+}
